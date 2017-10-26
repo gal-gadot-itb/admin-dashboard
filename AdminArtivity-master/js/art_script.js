b8dbcb3834a0bb5a03679fea6e036c3250426e45
Zulva Fachrina <zfachrina@gmail.com>
2017-10-27 04:02:02 +0700

    User & POI admin page

---
+++ AdminArtivity-master/js/art_script.js
@@ -0,0 +1,35 @@
+$(function () {
+    $('.js-basic-example').DataTable({
+        responsive: true
+    });
+
+    //Exportable table
+    $('.js-exportable').DataTable({
+        dom: 'Bfrtip',
+        responsive: true,
+        buttons: [
+            'copy', 'csv', 'excel', 'pdf', 'print'
+        ]
+    });
+
+    $('.count-to').countTo();
+
+    $('.delete-button').on('click', function () {
+            swal({
+            title: "Are you sure?",
+            text: "You will not be able to recover this user!",
+            type: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#DD6B55",
+            confirmButtonText: "Yes, delete it!",
+            closeOnConfirm: false
+        }, function () {
+            swal("Deleted!", "The user has been deleted.", "success");
+        });
+    });
+
+    $('#gallery').lightGallery({
+        thumbnail: true,
+        selector: '.image'
+    });
+});
